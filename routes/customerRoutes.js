f4b73e77e8e9e9ceb2f12e4a1d946c3c2d6547d7
Nitish <162097372+Nitixh001@users.noreply.github.com>
2025-05-12 11:28:30 +0530

    Add files via upload

---
+++ routes/customerRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const {
+    registerCustomer,
+     allCustomer, 
+     singleCustomer}
+      = require("../controllers/customerController");
+      //[POST]: register customer
+    router.post("/register",registerCustomer);
+
+    //GET getall customer
+    router.get("/customer", allCustomer); 
+
+    //GET single customer
+    router.get("/:id", singleCustomer);
+    module.exports = router;
