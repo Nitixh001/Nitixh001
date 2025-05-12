c3d0e53ec6cefd64210a301ba5b549be03fb9bf2
Nitish <162097372+Nitixh001@users.noreply.github.com>
2025-05-12 11:23:57 +0530

    Add files via upload

---
+++ controllers/customerController.js
@@ -0,0 +1,35 @@
+const Customer = require("../models/customer");
+exports.registerCustomer = async(req,res) => {
+    try{
+        const { name, email, contact, address, gender, userId} = req.body;
+        const customer = new Customer ({ name, email, contact, address, gender, userId});
+        await customer.save();
+        res.status(200).json({success: true, data: customer});
+    }catch(error){
+        res.status(500).json({success: false, message: error.message});
+    }
+};
+
+//get All customer
+
+exports.allCustomer = async(req,res)=>{
+    try{
+        const customers = await Customer.find({}).populate("userId", "name email").exec();
+        res.status(200).json({success: true, data: customers});
+    }catch(error){
+        console.error("Error fetching customers:", error)
+        res.status(500).json({success: false, message: error.message});
+    }
+};
+
+
+//get single customer
+exports.singleCustomer = async(req,res)=>{
+    try{
+        const customer = await Customer.findById(req.params.id).populate("userId", "name email");
+        if(!customer) return res.status(404).json({success: false,message: "Customer not found"});
+        res.status(200).json({success:true, data: customer});
+    }catch(error){
+      res.status(500).json({success: false, message: error.message});  
+    }
+};
